678c5cbc46b786fb6d89982148a940f8dcc25514
igor <igor.p.almeida@gmail.com>
2015-04-24 11:31:43 -0300

    all testes being created via grunt.

--- gruntfile.js
+++ gruntfile.js
@@ -11,77 +11,64 @@ module.exports = function(grunt) {
 			'test-almond': {
 				'src': ['inc/header.html', 'inc/scripts-almond.html','inc/body.html'],
 				'dest' : 'test/index-almond.html'
+			},
+			'test-pack': {
+				'src': ['inc/header.html', 'inc/scripts-concat.html','inc/body.html'],
+				'dest' : 'test/index-pack.html'
+			},
+			'pack-cache' : {
+				'src' : ['dist/js/hyojun.fuzzy-finder-almond.min.js','test/hyojun.fuzzy-finder-plugin.js'],
+				'dest' : 'test/hyojun.fuzzy-finder.pack.js'
 			}
 		},
 
 		'requirejs' : {
-			'dev': {
-				'options': {
-					'optimize' : 'none',
-					'baseUrl': "js",
-					'name': "fuzzy-finder/controller",
-					'out': "dist/js/fuzzy-finder.min.js"
-				}
-			},
 			'dist-amd': {
 				'options': {
+					'optimize' : grunt.option('uglify') || 'uglify2',
 					'baseUrl': "js",
-					'name': "fuzzy-finder/controller",
-					'out': "dist/js/fuzzy-finder.min.js"
+					'name': "hyojun.fuzzy-finder/controller",
+					'out': "dist/js/hyojun.fuzzy-finder.min.js"
 				}
 			},
 			'dist-almond': {
 				'options': {
+					'optimize' : grunt.option('uglify') || 'uglify2',
 					'baseUrl': "js",
 					'include' : ['lib/almond-0.3.1'],
-					'name': "fuzzy-finder/controller",
-					'out': "dist/js/fuzzy-finder-almond.min.js"
+					'name': 'hyojun.fuzzy-finder/controller',
+					'out': 'dist/js/hyojun.fuzzy-finder-almond.min.js'
 				}
 			}
 		},
 
-		"sass" : {
-			"options": {
-				"sourceMap": false
+		'sass' : {
+			'options': {
+				'sourceMap': false
 			},
-			"dist": {
-				"files": {
-					"dist/css/fuzzy-finder.css" : "sass/fuzzy-finder.scss"
+			'dist': {
+				'files': {
+					'dist/css/hyojun.fuzzy-finder.css' : 'sass/fuzzy-finder.scss'
 				}
 			}
 		},
 
 		'fuzzy-finder-cache' : {
-			'guideline' : {
-				'out' : 'test/fuzzy-finder-plugin.js',
-				'pages' : ['guideline/guideline-pages/listas.jsp', 'guideline/mockup/troque.jsp'],
+			'bbc-homepage' : {
+				'out' : 'test/hyojun.fuzzy-finder-plugin.js',
+				'pages' : ['http://www.bbc.com/news/technology'],
 				'rules' : {
 					'link' : {
-						'query' : ".gl-header, .gl-mockup-link, h3.gl, h4.gl, h5.gl, h6.gl",
+						'query' : '.title-link',
 						'get' : function($ele, blob){
-							var id = $ele.attr("id");
-
-							if ($ele.get().name === 'a'){
-								blob.h = $ele.attr('href');
-							} else if (!!id && id.length){
-								blob.h = blob.h + "#" + id;
-							}
-
-							blob.c = $ele.text().replace(/[\n|\t]/,"");
+							blob.c = $ele.text().trim();
+							blob.h = $ele.attr('href');
 							return blob;
-						}
+						},
 					},
 					'template' : {
-						'query' : 'script[type="text/x-mustache-template"]',
+						'query' : 'script[type="text/template"]',
 						'get' : 'id'
-					},
-					'data-service' : {
-						'query' : '[data-service]',
-						'get' : 'data-service'
-					},
-					'data-plugin' : {
-						'query' : '[data-plugin]',
-						'get' : 'data-plugin'
 					}
 				}
 			}
@@ -92,6 +79,8 @@ module.exports = function(grunt) {
 	grunt.loadTasks('node_modules/grunt-contrib-concat/tasks');
 	grunt.loadTasks('node_modules/grunt-sass/tasks');
 	grunt.loadTasks('node_modules/hyojun.grunt.fuzzy-finder-cache/tasks');
-	grunt.registerTask('default',['sass', 'requirejs:dist-amd', 'requirejs:dist-almond', 'fuzzy-finder-cache']);
+
+	grunt.registerTask('test', ['requirejs', 'concat']);
+	grunt.registerTask('default',['sass', 'fuzzy-finder-cache', 'test']);
 };
 
